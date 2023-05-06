91e1ff8d0a6cd1f2385e93991a3ef344bbd8e041
aagic1 <aagic1@etf.unsa.ba>
2023-05-06 03:22:26 +0200

    Code refactoring

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (() => {
+const GameBoard = () => {
   let board = new Array(9).fill(null);
 
   const getBoard = () => board;
@@ -12,16 +12,84 @@ const gameBoard = (() => {
   };
 
   return { getBoard, addMark, resetBoard };
-})();
+};
 
-const displayController = (() => {
+const Player = (name, mark) => {
+  const getMark = () => mark;
+  const getName = () => name;
+
+  return { getMark, getName };
+};
+
+const GameController = (name1 = 'player1', name2 = 'player2') => {
+  const player1 = Player(name1, 'X');
+  const player2 = Player(name2, 'O');
+  const gameBoard = GameBoard();
+  let turnsPlayed = 0;
+  let winner = null;
+  let draw = false;
+
+  const isDraw = () => draw;
+
+  const getWinner = () => winner;
+
+  const getActivePlayer = () => {
+    return turnsPlayed % 2 == 0 ? player1 : player2;
+  };
+
+  const playMove = position => {
+    const activePlayerMark = getActivePlayer().getMark();
+    gameBoard.addMark(position, activePlayerMark);
+
+    turnsPlayed++;
+    if (hasWon(activePlayerMark)) {
+      winner = activePlayerMark;
+    } else if (turnsPlayed == 9) {
+      draw = true;
+    }
+  };
+
+  const hasWon = mark => {
+    const board = gameBoard.getBoard();
+    return (
+      (board[0] === mark && board[0] === board[1] && board[0] === board[2]) ||
+      (board[3] === mark && board[3] === board[4] && board[3] === board[5]) ||
+      (board[6] === mark && board[6] === board[7] && board[6] === board[8]) ||
+      (board[0] === mark && board[0] === board[3] && board[0] === board[6]) ||
+      (board[1] === mark && board[1] === board[4] && board[1] === board[7]) ||
+      (board[2] === mark && board[2] === board[5] && board[2] === board[8]) ||
+      (board[0] === mark && board[0] === board[4] && board[0] === board[8]) ||
+      (board[2] === mark && board[2] === board[4] && board[2] === board[6])
+    );
+  };
+
+  const resetGame = () => {
+    gameBoard.resetBoard();
+    turnsPlayed = 0;
+    winner = null;
+    draw = false;
+  };
+
+  return {
+    playMove,
+    resetGame,
+    isDraw,
+    getWinner,
+    getActivePlayer,
+    getBoard: gameBoard.getBoard,
+  };
+};
+
+const DisplayController = (() => {
+  const game = GameController();
   const restartButton = document.querySelector('.restart-button');
   const boardElement = document.querySelector('.board-container');
   const statusElement = document.querySelector('.status-container');
   const squareElements = document.querySelectorAll('.square');
 
   restartButton.addEventListener('click', e => {
-    gameController.resetGame();
+    game.resetGame();
+    updateScreen();
   });
 
   squareElements.forEach(square => {
@@ -31,21 +99,33 @@ const displayController = (() => {
       if (currentSquare.textContent) {
         return;
       }
-      gameController.playMove(position);
+      game.playMove(position);
+      updateScreen();
     });
   });
 
-  const markSquare = (position, mark) => {
-    squareElements[position].textContent = mark;
-  };
+  const updateScreen = () => {
+    const draw = game.isDraw();
+    const winner = game.getWinner();
+    const activePlayer = game.getActivePlayer();
 
-  const gameOver = message => {
-    if (message === 'draw') {
+    if (draw) {
       setDrawMessage();
+      blockBoard();
+    } else if (winner !== null) {
+      setWinnerMessage(winner);
+      blockBoard();
     } else {
-      setWinnerMessage(message);
+      updateTurnMessage(activePlayer.getMark());
+      unblockBoard();
     }
-    blockBoard();
+
+    clearBoard();
+    game.getBoard().forEach((value, index) => {
+      if (value !== null) {
+        squareElements[index].innerHTML = value;
+      }
+    });
   };
 
   const blockBoard = () => {
@@ -56,7 +136,7 @@ const displayController = (() => {
     boardElement.classList.remove('unclickable');
   };
 
-  const emptyBoard = () => {
+  const clearBoard = () => {
     squareElements.forEach(square => (square.innerHTML = ''));
   };
 
@@ -76,69 +156,5 @@ const displayController = (() => {
     setGameStatusMessage(`It's a draw`);
   };
 
-  const resetBoard = startPlayerMark => {
-    emptyBoard();
-    updateTurnMessage(startPlayerMark);
-    unblockBoard();
-  };
-
-  const setCurrentPlayer = player => {
-    updateTurnMessage(player.getMark());
-  };
-
-  return { markSquare, gameOver, resetBoard, setCurrentPlayer };
-})();
-
-const Player = (name, mark) => {
-  const getMark = () => mark;
-  const getName = () => name;
-
-  return { getMark, getName };
-};
-
-const gameController = (() => {
-  const player1 = Player('player1', 'X');
-  const player2 = Player('player2', 'O');
-  let turnsPlayed = 0;
-
-  const getCurrentPlayer = () => {
-    return turnsPlayed % 2 == 0 ? player1 : player2;
-  };
-
-  const playMove = position => {
-    const currentPlayerMark = getCurrentPlayer().getMark();
-    gameBoard.addMark(position, currentPlayerMark);
-    displayController.markSquare(position, currentPlayerMark);
-
-    turnsPlayed++;
-    if (hasWon(currentPlayerMark)) {
-      displayController.gameOver(currentPlayerMark);
-    } else if (turnsPlayed == 9) {
-      displayController.gameOver('draw');
-    } else {
-      displayController.setCurrentPlayer(getCurrentPlayer());
-    }
-  };
-
-  const hasWon = mark => {
-    const board = gameBoard.getBoard();
-    return (
-      (board[0] === mark && board[0] === board[1] && board[0] === board[2]) ||
-      (board[3] === mark && board[3] === board[4] && board[3] === board[5]) ||
-      (board[6] === mark && board[6] === board[7] && board[6] === board[8]) ||
-      (board[0] === mark && board[0] === board[3] && board[0] === board[6]) ||
-      (board[1] === mark && board[1] === board[4] && board[1] === board[7]) ||
-      (board[2] === mark && board[2] === board[5] && board[2] === board[8]) ||
-      (board[0] === mark && board[0] === board[4] && board[0] === board[8]) ||
-      (board[2] === mark && board[2] === board[4] && board[2] === board[6])
-    );
-  };
-
-  const resetGame = () => {
-    turnsPlayed = 0;
-    gameBoard.resetBoard();
-    displayController.resetBoard(player1.getMark());
-  };
-
-  return { playMove, resetGame };
+  updateScreen();
 })();
